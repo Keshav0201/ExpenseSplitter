1f2a73d3c5ee5ee392fb2380a26e1e157081dd5e
Keshav Didwania <keshavdidwania20@gmail.com>
2026-09-12 23:11:48 +0530

    Old DB files deleated

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const app = require("./app");
-
-const PORT = process.env.PORT || 3016;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8787/api";
+const API_BASE_URL = "api.split.keshavdidwania.in/api";
 
 async function getAuthToken() {
   if (!window.Clerk) {
